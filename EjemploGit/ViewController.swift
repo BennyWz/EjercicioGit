4840e357c5b6cb09e027002d607ca90f43c5ef13
Benny Reyes <vn55bbh@M-C02G94MDMD6R.local>
2023-02-03 03:45:23 -0600

    Se agrega gitignore

--- EjemploGit/ViewController.swift
+++ EjemploGit/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        saluda()
+    }
+    
+    func saluda(){
         print("Hola mundo")
     }
 
